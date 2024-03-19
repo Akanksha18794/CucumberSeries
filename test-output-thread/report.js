57626c5507362b6b5d959ef3b0a6912588f5d9b8
Akanksha18794 <akanksha18794@gmail.com>
2024-03-19 15:01:08 -0400

    parallel run

---
+++ test-output-thread/report.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "de32cb5b-41c3-4aa2-aadf-87863a08d4da",
+    "feature": "Login page feature",
+    "scenario": "Login with correct credentials",
+    "start": 1710874757622,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1710874762368,
+    "className": "passed"
+  },
+  {
+    "id": "e41ba4cf-14e6-49fa-95f1-d8aa11947626",
+    "feature": "Home Page Feature",
+    "scenario": "HomePage section count",
+    "start": 1710874745146,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1710874750417,
+    "className": "failed"
+  },
+  {
+    "id": "74740009-98a3-48da-87c2-c56b42e9283e",
+    "feature": "Login page feature",
+    "scenario": "Login Page Title",
+    "start": 1710874750417,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1710874753093,
+    "className": "passed"
+  },
+  {
+    "id": "ddb2b259-5c4e-41d3-b9ee-50d64cbe9d61",
+    "feature": "Login page feature",
+    "scenario": "Forgot Password Link",
+    "start": 1710874753096,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1710874757618,
+    "className": "passed"
+  },
+  {
+    "id": "9d7d3811-8b55-4630-8335-a5b92bfdaccf",
+    "feature": "Home Page Feature",
+    "scenario": "HomePage Title",
+    "start": 1710874734555,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1710874745130,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 1,
+    "content": "Thread[main,5,main]"
+  }
+]);
+});
